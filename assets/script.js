f5b98a92b8d8e4597757747171f320b4e39eee51
Lakshay Gupta <lakshay0173.cse19@chitkara.edu.in>
2021-08-29 19:55:22 +0530

    added med and hard and remaining validation

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,77 @@
+var lvl=[]
+ var old=0;
 var wrong=[]
 var glowedr=[];
 var glowedc=[];
 var glowedg=[];
-function validate(){
-    console.log("validate")
+let ivalue=0;
+var selected=0;
+var oldtobe=0;
+function validate(i){
+  let tobecorrected=document.querySelector(`#cell-${i} input`).value 
+ // console.log(`tobe=${tobecorrected}`)
+  if(tobecorrected<10 && tobecorrected>0){
+    oldtobe=tobecorrected;
+   
+  document.querySelector(`#cell-${i} input`).value=tobecorrected;}
+   if(oldtobe!=0&&tobecorrected!=""&&tobecorrected<1)
+   document.querySelector(`#cell-${i} input`).value="";
+   if(oldtobe!=0&&tobecorrected!=""&&tobecorrected>9){
+    document.querySelector(`#cell-${i} input`).value=oldtobe;
+  }
+  
+    document.querySelector(`#cell-${i}`).classList.add("selected")
+    if(selected!=0 && selected!=i)
+    document.querySelector(`#cell-${selected}`).classList.remove("selected")
+    selected=i;
+    if(tobecorrected=="")
+    lvl[i-1]="-1"
+    else
+    lvl[i-1]=document.querySelector(`#cell-${i} input`).value 
+ for(let j in lvl){
+ 
+   let k=Number(j)+1;
+    ivalue=document.querySelector(`#cell-${i} input`).value
+    //console.log(`ivalue=${ivalue}`)
+   if(lvl[j]!=0 && lvl[j]==ivalue)
+   document.querySelector(`#cell-${k}`).classList.add("same")
+   if(old!=0&&old!=ivalue)
+   removesame(old)
+   }console.log(lvl)
+   console.log(old)
+    old=ivalue;
+    if (old=="")
+    old=-1
+    
+    
+    
+    
     rowsOK();
     columnsOK();
    groupsOK();
 
-    // if((rowsOK() && columnsOK()&& groupsOK())==true)
-    // document.getElementById("won").innerHTML("You won!!")
+    
 }
+function validat(){
+  console.log(lvl.length)                      
+  // &&!lvl.includes("-1") &&!lvl.includes(0)
+  console.log(wrong.length)
+if(wrong.length==0 && lvl.length==81 ){
+// document.getElementById("won").innerText="You won!!";
+//document.getElementById("play").classList.add("fadeout")
+document.getElementById("play").classList.toggle("hide")
+document.getElementById("won").classList.toggle("hide")
+document.getElementById("won").classList.toggle("fadein")
+}}
+function removesame(val){
+ 
+  for(let j in lvl){
+   
+    let k=Number(j)+1;
+   // let ivalue=document.querySelector(`#cell-${i} input`).value
+    if(lvl[j]!=0 && lvl[j]==val)
+  document.querySelector(`#cell-${k}`).classList.remove("same")
+}}
 function rowsOK(){
     
     for(let i=1;i<=9;i++){
@@ -29,21 +90,22 @@ function rowGlow(n){
   const first=(n-1)*9+1;
   const last=n*9;
   
-  console.log(n);
+  //console.log(n);
   for(let i=first ;i<=last;i++){
    // if(glowedr.length==9)
    //  document.querySelector(`#cell-${glowedr.shift()}`).classList.remove("glow")
     document.querySelector(`#cell-${i}`).classList.add("glow")
     glowedr.push(i);
   }
-  console.log(glowedr)}
+ // console.log(glowedr)
+}
 
 function rowOK(n){
     const rowN=[]
     const first=(n-1)*9+1;
     const last=n*9;
     
-    console.log(n);
+  //  console.log(n);
     for(let i=first ;i<=last;i++){
      // document.querySelector(`#cell-${i}`).classList.add("wrong")
         const v=document.querySelector(`#cell-${i} input`).value;
@@ -51,13 +113,13 @@ function rowOK(n){
         if(v!==""){
          if(rowN.includes(v)){
             wrong.push(i) 
-            console.log(i)
+          //  console.log(i)
             
             const existed=rowN.indexOf(v)
-            console.log(existed)
-            console.log(n)
-            console.log(9*(n-1))
-            console.log(existed+(9*(n-1)+1))
+         //   console.log(existed)
+          //  console.log(n)
+          //  console.log(9*(n-1))
+          //  console.log(existed+(9*(n-1)+1))
             wrong.push(existed+(9*(n-1)+1))
          }
          
@@ -68,18 +130,19 @@ function rowOK(n){
   for(let i in wrong){
     
        document.getElementById(`cell-${wrong[i]}`).classList.add("wrong")}
-       console.log(wrong)
+      // console.log(wrong)
      for(let j=0;j<wrong.length;j=j+2){
            const k=Number(j)+1;
-           console.log(document.querySelector(`#cell-${wrong[j]} input`).value)
-           console.log(document.querySelector(`#cell-${wrong[k]} input`).value)
+         //  console.log(document.querySelector(`#cell-${wrong[j]} input`).value)
+         //  console.log(document.querySelector(`#cell-${wrong[k]} input`).value)
            if(document.querySelector(`#cell-${wrong[j]} input`).value!=document.querySelector(`#cell-${wrong[k]} input`).value)
-           {console.log(wrong[j])
-            console.log(wrong[j+1])
+           {
+          //   console.log(wrong[j])
+         //   console.log(wrong[j+1])
                document.getElementById(`cell-${wrong[j]}`).classList.remove("wrong")
            document.getElementById(`cell-${wrong[k]}`).classList.remove("wrong")
            wrong.splice(j,2)
-           console.log(wrong)
+          // console.log(wrong)
         }
         }
   
@@ -89,7 +152,7 @@ function rowOK(n){
 function columnsOK(){
     
     for(let i=1;i<=9;i++){
-        console.log(i)
+     //   console.log(i)
         columnOK(i);
     }
     
@@ -106,7 +169,7 @@ function columnGlow(n){
     document.querySelector(`#cell-${i}`).classList.add("glow")
     glowedc.push(i);
   }
-  console.log(glowedc)
+  //console.log(glowedc)
   }
 
 
@@ -122,10 +185,10 @@ function columnOK(n){
         if(v!==""){
          if(colN.includes(v)){
             wrong.push(i) 
-            console.log(i)
+      //      console.log(i)
             
             const existed=colN.indexOf(v)
-            console.log(existed*9+n)
+         //   console.log(existed*9+n)
             wrong.push(existed*9+n)
          }
          
@@ -136,7 +199,7 @@ function columnOK(n){
   for(let i in wrong){
     
        document.getElementById(`cell-${wrong[i]}`).classList.add("wrong")}
-       console.log(wrong)
+      // console.log(wrong)
     //  for(let j=0;j<wrong.length;j=j+2){
     //        const k=Number(j)+1;
     //        console.log(document.querySelector(`#cell-${wrong[j]} input`).value)
@@ -159,12 +222,12 @@ function columnOK(n){
 function groupsOK(){
     
   for(let i=1;i<=9;i++){
-      console.log(i)
+    //  console.log(i)
       groupOK(i);
   }
 }
   function groupGlow(n){
-    console.log(n);
+  //  console.log(n);
     const first=3*(n-1)+18*Math.floor((n-1)/3)+1;
     const last=first+20;
     //let j=0;
@@ -193,15 +256,15 @@ function groupOK(n){
    // j=j+1;
    if(i==first+3||i==first+12)
    i=i+6;
-   console.log(i)
+  // console.log(i)
       const v=document.querySelector(`#cell-${i} input`).value;
       if(v!==""){
        if(groupN.includes(v)){
           wrong.push(i) 
-          console.log(i)
+       //   console.log(i)
           
           const existed=groupN.indexOf(v)
-          console.log(first+(Math.floor(existed/3))*9+(existed%3))
+        //  console.log(first+(Math.floor(existed/3))*9+(existed%3))
           wrong.push(first+(Math.floor(existed/3))*9+(existed%3))
        }
        
@@ -223,13 +286,13 @@ for(let j=0;j<wrong.length;j=j+2){
                  document.getElementById(`cell-${wrong[j]}`).classList.remove("wrong")
              document.getElementById(`cell-${wrong[k]}`).classList.remove("wrong")
              wrong.splice(j,2)
-             console.log(wrong)
+           //  console.log(wrong)
           }
           }
         }
 
 function setupeasy(){
-const easylvl=[
+  const easylvl=[
     [0, 3, 0, 6, 7, 8, 9, 0, 2],
     [6, 7, 2, 1, 0, 0, 3, 4, 0],
     [1, 0, 0, 3, 4, 2, 5, 6, 0],
@@ -239,35 +302,120 @@ const easylvl=[
     [9, 0, 1, 5, 0, 0, 2, 0, 4],
     [2, 8, 7, 0, 1, 9, 6, 3, 5],
     [3, 0, 0, 0, 8, 0, 1, 7, 0]
-]
+  ]
 var easy=[]
 for(i in easylvl){
    // console.log(easylvl[i])
      easy=[...easy,...easylvl[i]]
 }
-
+lvl=easy;
 //console.log(easy)
 for(let i in easy){
     
    // console.log(i);
     var j=Number(i)+1;
+    document.querySelector(`#cell-${j}`).classList.remove("same")
    // console.log(j);
     if(easy[i]=='0')
-    {document.querySelector(`#cell-${j} input`).setAttribute("value","")
-    
+    {document.querySelector(`#cell-${j} input`).value=""
+    document.querySelector(`#cell-${j}`).classList.remove("disabled")
+    document.querySelector(`#cell-${j} input`).disabled =false;
   
   }
     else{
        // console.log(document.querySelector(`#cell-${j} input`))
-   document.querySelector(`#cell-${j} input`).setAttribute("value",easy[i])
+   document.querySelector(`#cell-${j} input`).value=easy[i]
  //  document.querySelector(`#cell-${j} input`).setAttribute("name","disable")
    document.querySelector(`#cell-${j}`).classList.add("disabled")
    document.querySelector(`#cell-${j} input`).disabled =true;
 }}}
 
+function setupmed(){
+  const medlvl=[
+    [0, 3, 0, 6, 0, 8, 9, 0, 2],
+    [6, 0, 2, 1, 0, 0, 3, 4, 0],
+    [1, 0, 0, 3, 0, 2, 0, 6, 0],
+    [0, 5, 0, 7, 0, 1, 0, 2, 0],
+    [4, 0, 6, 0, 5, 0, 0, 0, 1],
+    [0, 0, 3, 0, 2, 0, 8, 0, 6],
+    [0, 0, 1, 5, 0, 0, 2, 0, 4],
+    [2, 8, 7, 0, 1, 0, 0, 0, 5],
+    [3, 0, 0, 0, 8, 0, 0, 7, 0]
+  ]
+var med=[]
+for(i in medlvl){
+   // console.log(easylvl[i])
+     med=[...med,...medlvl[i]]
+}
+lvl=med;
+//console.log(easy)
+for(let i in med){
+    
+   // console.log(i);
+    var j=Number(i)+1;
+    document.querySelector(`#cell-${j}`).classList.remove("same")
+   // console.log(j);
+    if(med[i]=='0')
+    {document.querySelector(`#cell-${j} input`).value=""
+    document.querySelector(`#cell-${j}`).classList.remove("disabled")
+    document.querySelector(`#cell-${j} input`).disabled =false;
+  
+  }
+    else{
+       // console.log(document.querySelector(`#cell-${j} input`))
+   document.querySelector(`#cell-${j} input`).value=med[i]
+ //  document.querySelector(`#cell-${j} input`).setAttribute("name","disable")
+   document.querySelector(`#cell-${j}`).classList.add("disabled")
+   document.querySelector(`#cell-${j} input`).disabled =true;
+}}}
+
+
+function setuphard(){
+  const hardlvl=[
+    [0, 0, 7,9 ,3, 0, 0, 0, 8],
+    [6, 8, 0, 0, 0, 5, 1, 9, 0],
+    [0, 0, 5, 6, 0, 0, 0, 0, 2],
+    [0, 0, 0, 4, 0, 0, 0, 8, 0],
+    [5, 9, 0, 1, 2, 6, 0, 3, 0],
+    [0, 6, 0, 0, 0, 0, 0, 0, 9],
+    [0, 7, 1, 3, 5, 4, 0, 2, 0],
+    [0, 0, 9, 7, 0, 0, 0, 5, 1],
+    [0, 0, 0, 0, 1, 0, 0, 7, 0]
+  ]
+var hard=[]
+for(i in hardlvl){
+   // console.log(easylvl[i])
+     hard=[...hard,...hardlvl[i]]
+}
+lvl=hard;
+//console.log(easy)
+for(let i in hard){
+    
+   // console.log(i);
+    var j=Number(i)+1;
+    document.querySelector(`#cell-${j}`).classList.remove("same")
+   // console.log(j);
+    if(hard[i]=='0')
+    {document.querySelector(`#cell-${j} input`).value=""
+    document.querySelector(`#cell-${j}`).classList.remove("disabled")
+    document.querySelector(`#cell-${j} input`).disabled =false;
+  
+  }
+    else{
+       // console.log(document.querySelector(`#cell-${j} input`))
+   document.querySelector(`#cell-${j} input`).value=hard[i]
+ //  document.querySelector(`#cell-${j} input`).setAttribute("name","disable")
+   document.querySelector(`#cell-${j}`).classList.add("disabled")
+   document.querySelector(`#cell-${j} input`).disabled =true;
+}}}
+
+
 for(let i=1;i<=81;i++)
-{//console.log(i);
-document.querySelector(`#cell-${i} input`).addEventListener("change",validate)
+{document.querySelector(`#cell-${i} input`).addEventListener("keyup",()=>{validate(i)})
+document.querySelector(`#cell-${i} input`).addEventListener("focus",()=>{validate(i)})
+//document.querySelector(`#cell-${i} input`).setAttribute("type","number")
+//document.querySelector(`#cell-${i} input`).setAttribute("max","9")
+//document.querySelector(`#cell-${i} input`).addEventListener("blur",()=>{removesame(i)})
 document.querySelector(`#cell-${i} input`).addEventListener("focus",()=>{rowGlow(Math.floor((i-1)/9)+1)})
 document.querySelector(`#cell-${i} input`).addEventListener("focus",()=>{columnGlow(Math.floor((i-1)%9)+1)})
 document.querySelector(`#cell-${i} input`).addEventListener("focus",()=>{groupGlow(Math.floor(((i-1)%9)/3)+1+3*Math.floor((i-1)/27))})
